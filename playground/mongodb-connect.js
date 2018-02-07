9c10df9d2ea9f2fa3bfb9658074d1705d843ca7f
ba030118 <ba030118@gmail.com>
2018-02-07 09:26:24 +0100

    Add delete  script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -23,16 +23,27 @@ MongoClient.connect('mongodb://localhost:27017/Users', (err, db)=> {
 
 	//insert new doc into Users(name, age, location)
 
-	db.collection('Users').insertOne({
-		name: 'Bartosz',
-		age: 36,
-		location: 'Berlin'
-	},  (err, result) => {
-		if(err) {
-			return console.log('Unable to insert todo', err);
-		}
-		console.log(JSON.stringify(result.ops, undefined, 2));
+	// db.collection('Users').insertOne({
+	// 	name: 'Bartosz',
+	// 	age: 36,
+	// 	location: 'Berlin'
+	// },  (err, result) => {
+	// 	if(err) {
+	// 		return console.log('Unable to insert todo', err);
+	// 	}
+	// 	console.log(JSON.stringify(result.ops, undefined, 2));
+	// });
+
+	//deleteMany
+	// db.collection('Users').deleteMany({name: 'Bartosz'}).then((result) => {
+	// 	console.log(result);
+	// });
+	db.collection('Users').findOneAndDelete({ _id: new ObjectID('5a781627966a8e2038b51ee4')}).then((result) => {
+		//console.log(result);
+		console.log(JSON.stringify(result, undefined, 2));
 	});
 
-	db.close();
+	//findOneAndDelete
+
+	// db.close();
 });
